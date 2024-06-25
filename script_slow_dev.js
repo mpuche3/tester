f2abbbe3074fa3c18ab933a758ed76a4dfeca21a
Miguel <22322402+mpuche3@users.noreply.github.com>
2024-06-25 11:17:46 +0000

    message

--- script_slow_dev.js
+++ script_slow_dev.js
@@ -357,6 +357,7 @@ function get_books(TEXTS_TRANS){
         `./${folder}/books/B012/B012_${xxxxxx}_ALL.txt`,
         `./${folder}/books/B013/B013_${xxxxxx}_ALL.txt`,
         `./${folder}/books/B014/B014_${xxxxxx}_ALL.txt`,
+        `./${folder}/books/B015/B015_${xxxxxx}_ALL.txt`,
     ]
     for (const url of urls){
         const text = get_text(url)
